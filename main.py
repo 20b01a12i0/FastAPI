22541a69ba4b18ca0e5f67dd164d4a4a73d5b089
20b01a12i0 <20b01a12i0@svecw.edu.in>
2023-12-03 20:05:12 +0530

    query params

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
+from fastapi import FastAPI, HTTPException,Query
+from typing import List
+from pydantic import BaseModel
+from uuid import uuid4
+import sqlite3
 
-from fastapi import FastAPI, HTTPException
+app = FastAPI()
+
+DATABASE_FILE = "carsdata.db"
+
+class CarBase(BaseModel):
+    brand: str
+    model: str
+    color: str
+    year_of_manufacture: str
+    fuel_type: str
+    is_rented: bool
+    is_available_to_buy: bool
+
+def create_table():
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS cars (
+            id TEXT PRIMARY KEY,
+            brand TEXT,
+            model TEXT,
+            color TEXT,
+            year_of_manufacture TEXT,
+            fuel_type TEXT,
+            is_rented BOOLEAN,
+            is_available_to_buy BOOLEAN
+        )
+    ''') 
+    conn.commit()
+    conn.close()
+
+create_table()
+
+from fastapi import FastAPI, HTTPException, Query
 from typing import List
 from pydantic import BaseModel
 from uuid import uuid4
@@ -62,16 +100,35 @@ def get_cars():
     car_list = [{"id": car[0], **dict(zip(CarBase.__annotations__, car[1:]))} for car in cars]
     return {"cars": car_list}
 
-@app.get("/api/cars/{car_id}", response_model=dict)
-def get_car(car_id: str):
+
+@app.get("/api/cars", response_model=dict)
+def get_cars(
+    id: str = Query(None),
+    brand: str = Query(None),
+    model: str = Query(None)
+):
     conn = sqlite3.connect(DATABASE_FILE)
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM cars WHERE id=?", (car_id,))
-    car = cursor.fetchone()
+    query = "SELECT * FROM cars WHERE 1=1"
+    params = []
+    if id:
+        query += " AND id = ?"
+        params.append(id)
+
+    if brand:
+        query += " AND brand = ?"
+        params.append(brand)
+
+    if model:
+        query += " AND model = ?"
+        params.append(model)
+
+    cursor.execute(query, params)
+    cars = cursor.fetchall() 
     conn.close()
-    if car:
-        return {"id": car[0], **dict(zip(CarBase.__annotations__, car[1:]))}
-    raise HTTPException(status_code=404, detail="Car not found")
+
+    car_list = [{"id": car[0], **dict(zip(CarBase.__annotations__, car[1:]))} for car in cars]
+    return {"cars": car_list}
 
 @app.put("/api/cars/{car_id}", response_model=dict)
 def update_car(car_id: str, car_update: CarBase):
@@ -84,13 +141,13 @@ def update_car(car_id: str, car_update: CarBase):
     ''', (car_update.brand, car_update.model, car_update.color, car_update.year_of_manufacture,
           car_update.fuel_type, car_update.is_rented, car_update.is_available_to_buy, car_id))
     conn.commit()
-
     cursor.execute("SELECT * FROM cars WHERE id=?", (car_id,))
     updated_car = cursor.fetchone()
     conn.close()
-
     if updated_car:
         return {"id": updated_car[0], **dict(zip(CarBase.__annotations__, updated_car[1:]))}
 
     raise HTTPException(status_code=404, detail="Car not found")
+
+
 
